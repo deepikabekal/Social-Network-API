79f2a7131a17783e35605cab3bb044845a9da6f2
Deepika Bekal <deepika.bekal2@gmail.com>
2021-06-24 17:59:49 +0400

    created User model

--- models/User.js
+++ models/User.js
@@ -14,5 +14,36 @@ const UserSchema = new Schema ({
         required : 'Email is required',
         unique : true,
         match : '^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$'
-    }
+    },
+
+    thoughts : [{
+        type : Schema.Types.ObjectId,
+        ref : 'Thought'
+    }],
+
+    friends : [{
+        type : Schema.Types.ObjectId,
+        ref : 'User'
+    }]
+
+}, 
+
+{
+    toJSON : {
+        virtuals : true
+    },
+    id : false
+}
+
+);
+
+//get total count of user's friends on retrieval
+UserSchema.virtua('friendCount').get(function () {
+    return this.friends.length;
 })
+
+//create User model using UserSchema
+const User = mongoose.model('User', UserSchema);
+
+//export the User model
+module.exports = User;
