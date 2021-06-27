71879524248fdc9369ce4623c0978bb3058ff952
Deepika Bekal <deepika.bekal2@gmail.com>
2021-06-27 23:06:26 +0400

    query to add and remove a friend added

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -87,6 +87,53 @@ const userController = {
     //remove a user's associated thoughts when deleted
 
 
+    //to add a new friend to a user's friend list
+    addFriend ({params, body}, res) {
+        User.findByIdAndUpdate (
+                {_id : params.userId },
+                { $push : {friends : params.friendId}},
+                {new : true}            
+        )
+        .then (dbUserData => {
+            if (!dbUserData)
+            {
+                res.status(404).json({message : 'User Id does not exists!'});
+                return;
+            }
+
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    },
+
+    //remove a friend from a user's friend list
+
+    removeFriend ({params}, res) {
+        User.findByIdAndUpdate(
+            {_id : params.userId},
+            {$pull : {friends : params.friendId}},
+            {new : true}        
+        )
+        .then (dbUserData => {
+            if (!dbUserData)
+            {
+                res.status(404).json({message : 'No User found with the id!'});
+                return;
+            }
+
+            res.json(dbUserData);
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+    }
+
+
 }
 
 module.exports = userController;
