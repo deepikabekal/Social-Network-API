9a2cbbdf50d62f4f6ee282a4b8049e460dc22ae4
Deepika Bekal <deepika.bekal2@gmail.com>
2021-06-28 00:14:50 +0400

    query to create new thought created

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -26,6 +26,31 @@ const thoughtController = {
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
+        });
+    },
+
+    //create a new thought
+    createThought ({params, body}, res) {
+        Thought.create (body)
+        .then (({_id}) => {
+            return User.findOneAndUpdate (
+                {username : body.username},
+                {$push : {thoughts : _id }},
+                { new  : true}
+            );
         })
+        .then (dbThoughtData => {
+            if (!dbThoughtData)
+            {
+                res.status(404).json({message : 'Thought Id does not exists!'});
+                return;
+            }
+
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
     }
 }
